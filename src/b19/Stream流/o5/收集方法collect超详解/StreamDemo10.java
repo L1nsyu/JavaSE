d33d89d216f85a69134b1668095fdf66a422688e
L1nsyu <2860410795@qq.com>
2025-09-21 17:16:39 +0800

    update

---
+++ src/b19/Stream流/o5/收集方法collect超详解/StreamDemo10.java
@@ -0,0 +1,94 @@
+package b19.Stream流.o5.收集方法collect超详解;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class StreamDemo10 {
+    public static void main(String[] args) {
+        //collect(Collector collector)      收集流中的数据,放到集合当中(List Set Map)⭐⭐⭐
+        //注意点:如果我们要收集到Map集合当中,键不能重复,否则会报错
+
+
+        ArrayList<String> list = new ArrayList<>();
+        Collections.addAll(list, "张无忌-男-15", "周芷若-女-15", "赵敏-女-15", "张三丰-男-15", "张强-男-15",
+                "张翠山-男-15", "谢广坤-男-15", "逍遥子-男-15");
+
+        //收集List当中⭐⭐
+        //需求:
+        //我要把所有的男性收集起来
+
+        List<String> newList1 = list.stream()
+                .filter(s -> "男".equals(s.split("-")[1]))
+                .collect(Collectors.toList());
+
+        System.out.println(newList1);
+
+        System.out.println("-------------------------");
+
+        //收集Set当中⭐⭐
+        //需求:
+        //我要把所有的男性收集起来
+        Set<String> newList2 = list.stream()
+                .filter(s -> "男".equals(s.split("-")[1]))
+                .collect(Collectors.toSet());
+
+        System.out.println(newList2);
+
+        System.out.println("-------------------------");
+
+        //收集Map当中⭐⭐⭐
+        //谁作为键,谁作为值
+        //我要把所有的男性收集起来
+        //键:姓名 ; 值:年龄
+
+        Map<String, Integer> map = list.stream()
+                .filter(s -> "男".equals(s.split("-")[1]))
+                /*
+                toMap : 参数一表示键的生成规则
+                      : 参数二表示值的生成规则
+
+                参数1:
+                        Function泛型1:表示流中的每一个数据的类型
+                        Function泛型2:表示Map集合中键的数据类型
+
+                        方法apply形参:依次表示流里面的每一个数据
+                            方法体:生成键的代码
+                            返回值:已经生成的键
+
+                参数2:
+                        Function泛型1:表示流中的每一个数据的类型
+                        Function泛型2:表示Map集合中键的数据类型
+
+                        方法apply形参:依次表示流里面的每一个数据
+                            方法体:生成值的代码
+                            返回值:已经生成的值
+                */
+                .collect(Collectors.toMap(new Function<String, String>() {
+                                              @Override
+                                              public String apply(String s) {
+                                                  //"张无忌-男-15"
+                                                  return s.split("-")[0];
+                                              }
+                                          },
+                        new Function<String, Integer>() {
+                            @Override
+                            public Integer apply(String s) {
+                                //"张无忌-男-15"
+                                return Integer.parseInt(s.split("-")[2]);
+                            }
+                        }
+                ));
+        System.out.println(map);
+
+
+        Map<String, Integer> map1 = list.stream()
+                .filter(s -> "男".equals(s.split("-")[1]))
+                .collect(Collectors.toMap(s ->
+                                s.split("-")[0]
+                        , s ->
+                                Integer.parseInt(s.split("-")[2])
+                ));
+        System.out.println(map1);
+    }
+}
