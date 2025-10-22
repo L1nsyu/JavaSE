d6d1a0a8d2a55e9138bfee9a738e0a5889360bb4
L1nsyu <2860410795@qq.com>
2025-10-22 09:17:44 +0800

    update

---
+++ src/b23/IO流/o11/IO流中不同JDK版本捕获异常的方式/ByteStreamDemo11.java
@@ -0,0 +1,12 @@
+package b23.IO流.o11.IO流中不同JDK版本捕获异常的方式;
+
+public class ByteStreamDemo11 {
+    public static void main(String[] args) {
+        //不要求掌握,能够了解并理解内容即可
+
+        //try...catch   不同版本下的异常处理
+
+        //try后面的小括号中写创建对象的代码,
+        //注意:只有实现了AutoCloseable接口的类,才能在小括号中创建对象.
+    }
+}
