011dd301a34c94de4edf58642865f049c9bdecb6
L1nsyu <2860410795@qq.com>
2025-10-22 20:36:01 +0800

    update

---
+++ src/b23/IO流/o12/字符集详解/CharSetDemo1.java
@@ -0,0 +1,21 @@
+package b23.IO流.o12.字符集详解;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class CharSetDemo1 {
+    public static void main(String[] args) throws IOException {
+
+        //字节流读取中文会出现乱码  ⭐
+
+        FileInputStream fis = new FileInputStream("C:\\Users\\lsy\\Desktop\\FileTest\\ramdom.txt");
+
+        int b;
+        while ((b = fis.read()) != -1) {
+            System.out.print((char) b);
+        }
+
+        fis.close();
+
+    }
+}
