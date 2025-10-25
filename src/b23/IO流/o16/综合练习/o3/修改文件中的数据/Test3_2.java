afc8078c893fd7ca17c98644b027f0c62ad640b1
L1nsyu <2860410795@qq.com>
2025-10-25 16:13:19 +0800

    update

---
+++ src/b23/IO流/o16/综合练习/o3/修改文件中的数据/Test3_2.java
@@ -0,0 +1,57 @@
+package b23.IO流.o16.综合练习.o3.修改文件中的数据;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.function.Function;
+
+public class Test3_2 {
+    public static void main(String[] args) throws IOException {
+/*
+    文本文件中有以下的数据:
+        2-1-9-4-7-8
+    将文件中的数据进行排序,变成以下的数据:
+        1-2-4-7-8-9
+*/
+
+        /*
+         * 细节1:
+         *       文件中的数据不要换行
+         *
+         * 细节2:
+         *       bom头
+         *
+         * */
+
+        //1.读取数据        2-1-9-4-7-8
+        FileReader fr = new FileReader("C:\\Users\\lsy\\Desktop\\NewFileTest\\a.txt");
+        StringBuilder sb = new StringBuilder();
+        int ch;
+        while ((ch = fr.read()) != -1) {
+            sb.append((char) ch);
+        }
+        fr.close();
+        System.out.println(sb);
+
+
+        //2.排序      2-1-9-4-7-8
+        Integer[] arr = Arrays.stream(sb.toString().split("-"))
+                .map(Integer::parseInt)
+                .sorted()
+                .toArray(Integer[]::new);
+
+        System.out.println(Arrays.toString(arr));
+
+        //3.写出
+        FileWriter fw = new FileWriter("C:\\Users\\lsy\\Desktop\\NewFileTest\\a.txt");
+        String s = Arrays.toString(arr).replace(", ", "-");
+        String result = s.substring(1, s.length() - 1);
+        fw.write(result);
+        fw.close();
+
+
+    }
+}
